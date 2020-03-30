ed493f76ea7f0cc881fef6eb1a7a58c73890236e
Jack Madden <jck13mad@gmail.com>
2020-03-30 17:16:13 +0000

    Automatically backed up by Learn

--- lib/artist.rb
+++ lib/artist.rb
@@ -17,8 +17,8 @@ class Artist
     @@song_count += 1
   end
 
-  def add_song_by_name(name, song)
-    song = Song.new(name, song)
+  def add_song_by_name(name)
+    song = Song.new(name)
     @songs << song
     song.artist = self
     @@song_count += 1
